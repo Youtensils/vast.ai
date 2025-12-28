6524cfb8e4526658a696e7c4bb9710b10fc14276
Elton MATHIAS <elton.mathias@sap.com>
2025-12-28 08:20:04 +0100

    Add Wan2.2-animate script

---
+++ template_installs/wan2.2-animate.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# WAN 2.2 Animate Install Script
+
+cd ComfyUI/models
+cd loras
+wget https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/LoRAs/Wan22_relight/WanAnimate_relight_lora_fp16.safetensors
+wget https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Lightx2v/lightx2v_I2V_14B_480p_cfg_step_distill_rank64_bf16.safetensors
+cd ../text_encoders/
+wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors
+cd ../vae
+wget https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
+cd ../clip_vision/
+wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors
+cd ../diffusion_models/
+wget https://huggingface.co/Kijai/WanVideo_comfy_fp8_scaled/resolve/main/Wan22Animate/Wan2_2-Animate-14B_fp8_e4m3fn_scaled_KJ.safetensors
+cd /workspace/
