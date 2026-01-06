2519335eba41a466bf02efb2ee29da839b092248
Elton MATHIAS <elton.mathias@sap.com>
2026-01-06 12:25:45 +0100

    Video Wan Vace Inpaint Install Script

---
+++ template_installs/video_wan_vace_inpaint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Video Wan Vace Inpaint Install Script
+# to run in Jupyter terminal:
+# bash <(wget -qO- https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/template_installs/video_wan_vace_inpaint.sh) 
+
+
+cd ComfyUI/models
+
+cd ../loras
+wget https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan21_CausVid_bidirect2_T2V_1_3B_lora_rank32.safetensors
+wget https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan21_CausVid_14B_T2V_lora_rank32.safetensors
+
+cd ../diffusion_models
+wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_vace_1.3B_fp16.safetensors
+wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_vace_14B_fp16.safetensors
+
+cd ../text_encoders
+wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors?download=true
+wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp16.safetensors
+
+cd ../vae
+wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
+
+cd /workspace 
