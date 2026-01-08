f1e6947378ad44ab9c0a907debc940de05ab6a61
Elton MATHIAS <elton.mathias@sap.com>
2026-01-08 15:57:39 +0100

    update with aria and download vaes

--- scripts/clone_instance/clone_local.sh
+++ scripts/clone_instance/clone_local.sh
@@ -11,10 +11,27 @@ apt-get update && apt-get install -y aria2
 mkdir -p $BASEDIR/YT/cloner
 cd $BASEDIR/YT/cloner
 
-#download listing file
-wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/custom_nodes.txt
+function download_from_file() {
+    FILE=$1
+    OUTPUT_PATH=$2
+    grep -v '^\s*#' $FILE | aria2c \
+        --continue \
+        --max-concurrent-downloads=16 \
+        --split=16 \
+        --max-connection-per-server=16 \
+        --min-split-size=1M \
+        --enable-http-pipelining=true \
+        --disable-ipv6=true \
+        --dir="$OUTPUT_PATH" \
+        -i -
+
+}
+
+
+
 
 #dowload custom nodes
+wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/custom_nodes.txt
 
 cat $BASEDIR/YT/cloner/custom_nodes.txt | while read line; do
     cd $BASEDIR/custom_nodes
@@ -22,4 +39,9 @@ cat $BASEDIR/YT/cloner/custom_nodes.txt | while read line; do
     git clone $line
     cd -
 done
+
+#download vaes
+wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/vae.txt
+download_from_file $BASEDIR/YT/cloner/vae.txt $BASEDIR/models/vae
+
 
