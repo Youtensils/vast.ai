2ecbc7f40689040560d0bc839b6637459774eba3
Elton MATHIAS <elton.mathias@sap.com>
2026-01-08 13:06:06 +0100

    add clone local

--- scripts/clone_instance/clone_local.sh
+++ scripts/clone_instance/clone_local.sh
@@ -3,33 +3,31 @@
 # to run in Jupyter terminal:
 # bash <(wget -qO- https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/template_installs/clone_local.sh) 
 
+BASEDIR=/workspace/ComfyUI/
+
 #install aria2 for faster downloads
 apt-get update && apt-get install -y aria2
 
-#navigate to ComfyUI directory
-cd /workspace/ComfyUI/
+
+mkdir -p $BASEDIR/YT/cloner
+cd $BASEDIR/YT/cloner
 
 #download listing file
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/listing.txt
+#download Manager custom_nodes catalog
+wget https://raw.githubusercontent.com/Comfy-Org/ComfyUI-Manager/refs/heads/main/custom-node-list.json
+
 
 #dowload custom nodes
-cat listing.txt | grep custom_nodes | cut -d "/" -f5 | sort | uniq | grep -v pycache | grep -v websocket_image > custom_nodes_list.txt
+cat listing.txt | grep custom_nodes | cut -d "/" -f5 | sort | uniq | grep -v pycache | grep -v websocket_image > custom-nodes_required.txt
+
+
+cat $BASEDIR/YT/cloner/custom-nodes_required.txt | while read line; do
+    node_url=$(cat custom-node-list.json | grep cg-use-everywhere | grep -v reference | cut -d "\"" -f0).git
+    cd $BASEDIR/ComfyUI/custom_nodes/
+    git clone $node_url
+    cd -
+done
 
 
 
-cd loras
-wget https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan21_CausVid_bidirect2_T2V_1_3B_lora_rank32.safetensors
-wget https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan21_CausVid_14B_T2V_lora_rank32.safetensors
-
-cd ../diffusion_models
-wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_vace_1.3B_fp16.safetensors
-wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_vace_14B_fp16.safetensors
-
-cd ../text_encoders
-wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors
-wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp16.safetensors
-
-cd ../vae
-wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
-
-cd /workspace 
