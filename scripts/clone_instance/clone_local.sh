9c5a70b36d3587ca7833f10d6088b980a1c495ed
Elton MATHIAS <elton.mathias@sap.com>
2026-01-12 15:03:06 +0100

    BR5

--- scripts/clone_instance/clone_local.sh
+++ scripts/clone_instance/clone_local.sh
@@ -38,73 +38,73 @@ cat $BASEDIR/YT/cloner/custom_nodes.txt | while read line; do
     cd -
 done
 
-dowload workflows
+#dowload workflows
 mkdir -p $BASEDIR/workflows
 cd $BASEDIR/workflows
 wget https://github.com/Youtensils/vast.ai/raw/refs/heads/main/scripts/clone_instance/workflows/workflows.tgz
 tar -xvzf workflows.tgz
 cd -
 
-download audio_encoders
+#download audio_encoders
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/audio_encoders.txt
 download_from_file $BASEDIR/YT/cloner/audio_encoders.txt $BASEDIR/models/audio_encoders
 
-download checkpoints
+#download checkpoints
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/checkpoints.txt
 download_from_file $BASEDIR/YT/cloner/checkpoints.txt $BASEDIR/models/checkpoints
 
-download clips
+#download clips
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/clips.txt
 download_from_file $BASEDIR/YT/cloner/clips.txt $BASEDIR/models/clips
 
-download clip_vision
+#download clip_vision
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/clip_vision.txt
 download_from_file $BASEDIR/YT/cloner/clip_vision.txt $BASEDIR/models/clip_vision
 
-download controlnet
+#download controlnet
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/controlnet.txt
 download_from_file $BASEDIR/YT/cloner/controlnet.txt $BASEDIR/models/controlnet
 
-download detection
+#download detection
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/detection.txt
 download_from_file $BASEDIR/YT/cloner/detection.txt $BASEDIR/models/detection
 
-download diffusers
+#download diffusers
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/diffusers.txt
 download_from_file $BASEDIR/YT/cloner/diffusers.txt $BASEDIR/models/diffusers
 
-download diffusion_models
+#download diffusion_models
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/diffusion_models.txt
 download_from_file $BASEDIR/YT/cloner/diffusion_models.txt $BASEDIR/models/diffusion_models
 
-download embeddings
+#download embeddings
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/embeddings.txt
 download_from_file $BASEDIR/YT/cloner/embeddings.txt $BASEDIR/models/embeddings
 
-download gligen
+#download gligen
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/gligen.txt
 download_from_file $BASEDIR/YT/cloner/gligen.txt $BASEDIR/models/gligen
 
-download latent_upscale_models
+#download latent_upscale_models
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/latent_upscale_models.txt
 download_from_file $BASEDIR/YT/cloner/latent_upscale_models.txt $BASEDIR/models/latent_upscale_models
 
-download loras
+#download loras
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/loras.txt
 download_from_file $BASEDIR/YT/cloner/loras.txt $BASEDIR/models/loras
 
-download model_patches
+#download model_patches
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/model_patches.txt
 download_from_file $BASEDIR/YT/cloner/model_patches.txt $BASEDIR/models/model_patches
 
-download sam2
+#download sam2
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/sam2.txt
 download_from_file $BASEDIR/YT/cloner/sam2.txt $BASEDIR/models/sam2
 
-download text_encoders
+#download text_encoders
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/text_encoders.txt
 download_from_file $BASEDIR/YT/cloner/text_encoders.txt $BASEDIR/models/text_encoders
 
-download vaes
+#download vaes
 wget https://raw.githubusercontent.com/Youtensils/vast.ai/refs/heads/main/scripts/clone_instance/models/vae.txt
 download_from_file $BASEDIR/YT/cloner/vae.txt $BASEDIR/models/vae
